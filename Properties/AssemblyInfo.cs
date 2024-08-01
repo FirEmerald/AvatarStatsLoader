023207f57e8512a7a7006a09a41d8cca9fdcac57
FirEmerald <mccaslinwalt@gmail.com>
2024-07-31 20:12:25 -0500

    Version bump to 1.3.0

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -34,7 +34,7 @@ using AvatarStatsLoader;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.3")]
-[assembly: AssemblyFileVersion("1.2.3")]
+[assembly: AssemblyVersion("1.3.0")]
+[assembly: AssemblyFileVersion("1.3.0")]
 
-[assembly: MelonInfo(typeof(AvatarStatsMod), "Avatar Stats Loader", "1.2.3", "FirEmerald", "https://bonelab.thunderstore.io/package/FirEmerald/AvatarStatsLoader/")] //TODO mod link
+[assembly: MelonInfo(typeof(AvatarStatsMod), "Avatar Stats Loader", "1.3.0", "FirEmerald", "https://bonelab.thunderstore.io/package/FirEmerald/AvatarStatsLoader/")] //TODO mod link
